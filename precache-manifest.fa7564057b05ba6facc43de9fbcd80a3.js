140cc7723dc8ea258b262f94f32ddd535a730cbc
htchung <htchung@gms.tku.edu.tw>
2020-10-14 18:51:28 +0800

    Updates

---
+++ precache-manifest.fa7564057b05ba6facc43de9fbcd80a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31d872d9a711bc2c9b0d9468e7db535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "223219d6de6cbab9ddf3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223219d6de6cbab9ddf3",
+    "url": "/static/js/main.21496c85.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
