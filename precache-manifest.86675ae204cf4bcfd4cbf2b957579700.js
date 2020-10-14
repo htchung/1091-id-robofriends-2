4e822723bd1153a6d715e80fee0d22be86a3e981
htchung <htchung@gms.tku.edu.tw>
2020-10-14 19:29:40 +0800

    Updates

---
+++ precache-manifest.86675ae204cf4bcfd4cbf2b957579700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab705a6816eeb7083bfa1ed1b359a5c9",
+    "url": "/1091-id-robofriends-2/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends-2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b59eea31e9a2956d13be",
+    "url": "/1091-id-robofriends-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends-2/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-id-robofriends-2/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59eea31e9a2956d13be",
+    "url": "/1091-id-robofriends-2/static/js/main.9a725ce2.chunk.js"
+  },
+  {
+    "revision": "ffbc1987bd535fdfba70",
+    "url": "/1091-id-robofriends-2/static/js/runtime-main.08b966c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa7564057b05ba6facc43de9fbcd80a3.js"
+  "/1091-id-robofriends-2/precache-manifest.86675ae204cf4bcfd4cbf2b957579700.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1091-id-robofriends-2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
